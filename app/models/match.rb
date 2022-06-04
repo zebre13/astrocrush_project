319e5d7c1e46b10d408e3d2efba5ef01258f6331
Maria Varela <leonor.varela91330@gmail.com>
2022-06-04 11:33:37 +0200

    add validations

--- app/models/match.rb
+++ app/models/match.rb
@@ -2,4 +2,7 @@ class Match < ApplicationRecord
   belongs_to :user
   belongs_to :chatroom
   belongs_to :mate, :foreign_key => :mate_id, :class_name => 'User'
+
+  validates :status, presence: true
+  validates :score, presence: true, uniqueness: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many_attached :photos
+
+  validates :username, presence: true
+  # validates :email, presence: true
+  validates :description, presence: true, uniqueness: true
+  validates :birth_date, presence: true
+  validates :birth_hour, presence: true
+  validates :birth_location, presence: true
 end

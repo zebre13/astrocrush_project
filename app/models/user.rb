9010bfb0e6fbe1cd4d303e47faecaee023e869ed
Maria Varela <leonor.varela91330@gmail.com>
2022-06-04 12:35:52 +0200

    validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,24 @@ class User < ApplicationRecord
   has_many_attached :photos
 
   validates :username, presence: true
-  # validates :email, presence: true
+  # validates :email, presence: true, /.+@.+\.\w{2,3}/
+  # validates_format_of :email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a- z]{2,})$/i
   validates :description, presence: true, uniqueness: true
   validates :birth_date, presence: true
+  # validates_date :date_of_birth, :after => Proc.new { Date.today }
   validates :birth_hour, presence: true
   validates :birth_location, presence: true
+  validates :gender, presence: true
+  validates :looking_for, presence: true
+  validates :star_sign, presence: true
+
+  validate :user_is_adult
+
+  private
+
+  def user_is_adult
+    if Date.today.year - birth_date.year < 18
+      self.errors.add(:birth_date, "user must be aged above 18")
+    end
+  end
 end

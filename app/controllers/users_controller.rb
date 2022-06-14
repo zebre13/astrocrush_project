a4c95ffe314836b07ddc173ab643ef50d2f67dc8
Etienne de Dianous <etiennededi@hotmail.fr>
2022-06-14 22:42:24 +0200

    swipe working, not wired to backend

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,4 +17,12 @@ class UsersController < ApplicationController
   def show
     @mate = User.find(params[:id])
   end
+
+  def test
+    @users_by_gender = User.where(gender: current_user.looking_for).where.not(id: current_user.id)
+    # On rejette tous les users qui sont dans les matchs du current user.
+    @users = @users_by_gender.reject do |user|
+      Match.where("(user_id = ? AND status = 0) OR (mate_id = ? AND status IN (1, 2))", current_user.id, current_user.id).pluck(:mate_id, :user_id).flatten.include?(user.id)
+    end
+  end
 end
--- app/javascript/controllers/swipe_controller.js
+++ app/javascript/controllers/swipe_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 import Hammer from 'hammerjs';
 import UseStrictPlugin from "webpack/lib/UseStrictPlugin"
 export default class extends Controller {
-  static targets = ["card","user", "users", "emptyUser", "buttons", "swipeLeftBtn", "swipeRightBtn"]
+  static targets = ["user"]
 
   connect() {
     console.log("coucou")
@@ -11,19 +11,36 @@ export default class extends Controller {
   }
 
   _setupDragAndDrop() {
+    const maxAngle = 42;
+    const smooth = 0.3;
+    const threshold = 42;
+    const thresholdMatch = 150;
+
     this.userTargets.forEach((profile) => {
       const hammertime = new Hammer(profile);
 
       hammertime.on('pan', (e) => {
+        e.target.classList.remove('profile-back');
         let posX = e.deltaX;
-        let posY = e.deltaY;
+        let posY = Math.max(0, Math.abs(posX * smooth) - 42);
+        let angle = Math.min(Math.abs(e.deltaX * smooth / 100), 1) * maxAngle;
 
-        const rotate = e.deltaX * 0.03 * e.deltaY / 80;
-        e.target.style.transform = `translate(${e.deltaX}px, ${e.deltaY}px) rotate(${rotate}deg)`;
+        if(e.deltaX < 0) {
+          angle *= -1;
+        }
+
+        e.target.style.transform = `translate(${posX}px, ${posY}px) rotate(${angle}deg)`;
 
         if (e.isFinal) {
 
           e.target.style.transform = ``;
+          if(posX > thresholdMatch){
+            e.target.classList.add('profile-match');
+          } else if (posX < -thresholdMatch){
+            e.target.classList.add('profile-next');
+          } else {
+            e.target.classList.add('profile-back');
+          }
         }
       })
 

c21b971a2170cacefc3fb79dfdf6bea6b7a4e5f4
Boris <95616326+zebre13@users.noreply.github.com>
2022-06-15 17:14:15 +0200

    generated astrobaord route and view

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :matches
   get 'dashboard', to: 'users#dashboard'
+  get 'astroboard', to: 'users#astroboard'
   post '/create_denied_match', to: 'matches#create_denied_match', as: 'create_denied_match'
   # get '/users/:id', to: 'users#show'
   resources :users, only: [:show, :edit]

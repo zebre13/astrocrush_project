52cde18a9096ddabc548da23edae022f4cefbe3d
Ghita Aaddaj <aa.ghita@gmail.com>
2022-09-02 16:38:11 +0200

    score get the ParentElement

--- app/javascript/controllers/score_controller.js
+++ app/javascript/controllers/score_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["birth_day", "birth_hour", "field", "latitude", "longitude", "gender", "current-latitude", "current-longitude", "current-birthhour", "current-birthdate", "current-gender"]
+  static targets = ["birth_day", "birth_hour", "field", "latitude", "longitude", "gender"]
 
   connect() {
     console.log("Hello from our Score controller")
@@ -13,49 +13,14 @@ export default class extends Controller {
 
   match_percentage(event){
     event.preventDefault()
-    console.log(event)
+    console.log(event.target.parentElement)
 
-    // if (currentUser.gender == 1) {
-      // const m_birth_day = currentUser.birth_day
-      // const m_birth_hour = currentUser.birth_hour
-      // const m_birth_location = currentUser.birth_location
-      // const m_birth_country = currentUser.birth_country
-      // const f_birth_day = this.birth_dayTarget.value
-      // const f_birth_hour = this.birth_hourTarget.value
-      // const f_birth_location = this.latitudeTarget.value
-      // const f_birth_country = this.longitudeTarget.value
-    // }
-  //   console.log(birthday)
-  //   console.log(birth_hour)
-  //   console.log(birth_location)
-  //   console.log(birth_country)
     console.log(this.birth_dayTarget.value)
     console.log(this.birth_hourTarget.value)
     console.log(this.latitudeTarget.value)
     console.log(this.longitudeTarget.value)
     console.log(this.genderTarget.value)
-    console.log(this.current-latitudeTarget.value)
-    console.log(this.current-longitudeTarget.value)
-    console.log(this.current-birthhourTarget.value)
-    console.log(this.current-birthdateTarget.value)
-    console.log(this.current-genderTarget.value)
 
-
-    // je veux un nouvel user avec les données + haut
-    // je veux comparer le pourcentage match entre mon current user et ce user
-
-
-    // const url = this.birthday.action
-    // fetch(url, options)
-    // .then(response => response.json())
-    // .then(data => {
-    //   console.log(data)
-    // })
-
-    // var input_code = (birthday, birth_hour, birth_location, birth_country, current_user.birth_date, current_user.birth_hour, current_user.birth_location, current_user.birth_country);
-    // this.getJSON('match_percentage/'+input_code+'/<%= @code %>').done(function(data){
-    //   alert(data);
-    // })
   };
 
   initMap(){
@@ -70,8 +35,5 @@ export default class extends Controller {
     let place = this.autocomplete.getPlace()
     this.latitudeTarget.value = place.geometry.location.lat()
     this.longitudeTarget.value = place.geometry.location.lng()
-    // this.countryTarget.value = place.address_components[3].short_name
-    // this.cityTarget.value = place.address_components[0].long_name
-    // this.utcoffsetTarget.value = place.utc_offset_minutes
   }
 }

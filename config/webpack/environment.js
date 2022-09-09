69f450c728d26d72f78edba1493d997281a709b2
Ghita Aaddaj <aa.ghita@gmail.com>
2022-09-09 14:39:50 +0200

    score without env

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,4 +2,15 @@ const { environment } = require('@rails/webpacker')
 
 // Preventing Babel from transpiling NodeModules packages
 environment.loaders.delete('nodeModules');
+
+//jquery
+const webpack = require('webpack')
+
+environment.plugins.prepend('Provide',
+new webpack.ProvidePlugin({
+  $: 'jquery/src/jquery',
+  jQuery: 'jquery/src/jquery'
+})
+)
+
 module.exports = environment

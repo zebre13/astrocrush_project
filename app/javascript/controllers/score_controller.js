339062aa69d954129b4d4305db1b0231aacb79dc
Ghita Aaddaj <aa.ghita@gmail.com>
2022-09-14 18:04:05 +0200

    test under progress

--- app/javascript/controllers/score_controller.js
+++ app/javascript/controllers/score_controller.js
@@ -5,9 +5,10 @@ export default class extends Controller {
   static values = {
     currentBirthDate: String,
     currentBirthHour: String,
-    currentLatitude: String,
-    currentLongitude: String,
-    currentGender: String
+    currentLatitude: Number,
+    currentLongitude: Number,
+    currentGender: Number,
+    currentTimezone: Number,
   }
 
   connect() {
@@ -18,9 +19,10 @@ export default class extends Controller {
     }
   }
 
-
   match_percentage(event){
     event.preventDefault()
+
+    // function request pour l'appel d'API
 
     var request = (resource, data) => {
       const userId = '';
@@ -37,6 +39,19 @@ export default class extends Controller {
         data:JSON.stringify(data)
       });
     }
+
+    // récupère les données du current user
+    var currentData = {
+      'day': parseInt(this.currentBirthDateValue.split('-')[2]),
+      'month': parseInt(this.currentBirthDateValue.split('-')[1]),
+      'year': parseInt(this.currentBirthDateValue.split('-')[0]),
+      'hour': parseInt(this.currentBirthHourValue.split(' ')[1].split(':')[0]),
+      'min': parseInt(this.currentBirthHourValue.split(' ')[1].split(':')[1]),
+      'lat': parseFloat(this.currentLatitudeValue),
+      'lon': parseFloat(this.currentLongitudeValue),
+      'tzone': parseFloat(this.currentTimezoneValue)
+    };
+
     // récupère les données du crush
     var crushData = {
       'day': parseInt(this.birthDateTarget.value.split('-')[2]),
@@ -49,92 +64,80 @@ export default class extends Controller {
       'tzone': 0.0
     };
 
-    // appel de l'api pour le timezone crush
-    let resource = 'timezone_with_dst';
-    let data = {
-      'latitude': crushData['lat'],
-      'longitude': crushData['lon'],
-      'date': `${crushData['month']}-${crushData['day']}-${crushData['year']}`,
-    }
-
-    request(resource, data).then((resp) => {
-      const response = parseFloat(resp['timezone'])
-      console.log(resp)
-      console.log(response)
-      crushData['tzone'] = response
-    }, (err) => {
-      console.log(err);
-    });
-
-    // récupère les données du current user
-    var currentData = {
-      'day': parseInt(this.currentBirthDateValue.split('-')[2]),
-      'month': parseInt(this.currentBirthDateValue.split('-')[1]),
-      'year': parseInt(this.currentBirthDateValue.split('-')[0]),
-      'hour': parseInt(this.currentBirthHourValue.split(' ')[1].split(':')[0]),
-      'min': parseInt(this.currentBirthHourValue.split(' ')[1].split(':')[1]),
-      'lat': parseFloat(this.currentLatitudeValue),
-      'lon': parseFloat(this.currentLongitudeValue),
-      'tzone': 1.5
-    };
-
-    // appel de l'api pour timezone current
-    resource = 'timezone_with_dst';
-    data = {
-      'latitude': currentData['lat'],
-      'longitude': currentData['lon'],
-      'date': `${currentData['month']}-${currentData['day']}-${currentData['year']}`,
-    }
-
-    request(resource, data).then((resp) => {
-      const response = parseFloat(resp['timezone'])
-      console.log(resp)
-      console.log(response)
-      currentData['tzone'] = response
-    }, (err) => {
-      console.log(err);
-    });
-
-    console.log(currentData)
-    console.log(crushData)
-    // transforme les clés du current user et du crush en male ou female en fonction du sexe du current user
-
     var mData = {}
     var fData = {}
-    if(this.currentGenderValue === 1){
-      Object.entries(currentData).forEach(([k, v]) => {
-        const el = {[`m_${k}`]: v};
-        Object.assign(mData, el)
-      });
-      Object.entries(crushData).forEach(([k, v]) => {
-        const el = {[`f_${k}`]: v};
-        Object.assign(fData, el)
-      });
-    } else {
-      Object.entries(crushData).forEach(([k, v]) => {
-        const el = {[`m_${k}`]: v};
-        Object.assign(mData, el)
-      });
-      Object.entries(currentData).forEach(([k, v]) => {
-        const el = {[`f_${k}`]: v};
-        Object.assign(fData, el)
+
+    // function appel de l'api pour le timezone crush
+    const timeZone = () => {
+      const resource = 'timezone_with_dst';
+      const data = {
+        'latitude': crushData['lat'],
+        'longitude': crushData['lon'],
+        'date': `${crushData['month']}-${crushData['day']}-${crushData['year']}`,
+      }
+
+      request(resource, data).then((resp) => {
+        crushData['tzone'] = parseFloat(resp['timezone']);
+        return crushData
+      }, (err) => {
+        console.log(err);
+      })
+    }
+
+    // function transforme les clés du current user et du crush en male ou female en fonction du sexe du current user
+    var setGender = () => {
+      if(this.currentGenderValue === 1){
+        Object.entries(currentData).forEach(([k, v]) => {
+          const el = {[`m_${k}`]: v};
+          Object.assign(mData, el)
+        });
+        Object.entries(crushData).forEach(([k, v]) => {
+          const el = {[`f_${k}`]: v};
+          Object.assign(fData, el)
+        });
+      } else {
+        Object.entries(crushData).forEach(([k, v]) => {
+          const el = {[`m_${k}`]: v};
+          Object.assign(mData, el)
+        });
+        Object.entries(currentData).forEach(([k, v]) => {
+          const el = {[`f_${k}`]: v};
+          Object.assign(fData, el)
+        });
+      } var data = {...mData, ...fData};
+      return data
+    }
+
+    // function appel de l'api pour le match_percentage
+    var match = () => {
+      const resource = 'match_percentage';
+      // const data = {...mData, ...fData};
+
+      request(resource, data).then((resp) => {
+        console.log(resp);
+        this.scoreAlertTarget.classList.remove("d-none");
+        this.scoreAlertTarget.innerText = `${resp.match_percentage} %`;
+      }, (err) => {
+        console.log(err);
       });
     }
 
-    resource = 'match_percentage';
-    data = {...mData, ...fData};
-
-    console.log(data)
-
-    request(resource, data).then((resp) => {
+    console.log(timeZone())
+    // appel de l'api
+    timeZone().then((resp) => {
       console.log(resp)
-      this.scoreAlertTarget.classList.remove("d-none");
-      this.scoreAlertTarget.innerText = `${resp.match_percentage} %`
+      setGender()
+      .then((resp) => {
+        console.log(resp)
+        match()
+      }, (err) =>{
+        console.log(err);
+      })
     }, (err) => {
       console.log(err);
-    });
-
+    })
   };
+
 
 
   // google map

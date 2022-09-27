57fe3dc0fb95e7dd5cd98ef5b4a6ab3f2319e9cd
Ghita Aaddaj <aa.ghita@gmail.com>
2022-09-27 16:09:32 +0200

    session_controller modified

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-require_relative '../app/services/astrology_api.rb'
+require_relative '../../../app/services/astrology_api.rb'
 
 class Users::SessionsController < Devise::SessionsController
   API_CALL = AstrologyApi.new(ENV["API_UID"], ENV["API_KEY"])

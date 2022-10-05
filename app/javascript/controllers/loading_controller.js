2b12554085c3826b5a381ac86b11cfae8f098f53
Ghita Aaddaj <aa.ghita@gmail.com>
2022-10-05 11:57:41 +0200

    loading modal added

--- app/javascript/controllers/loading_controller.js
+++ app/javascript/controllers/loading_controller.js
@@ -1,12 +1,17 @@
 import { Controller } from "stimulus"
+import { Modal } from "bootstrap";
 
 export default class extends Controller {
+  static targets = ['modal']
 
   connect() {
     this.outputTarget.textContent = 'Hello, Stimulus!'
   }
 
-  load() {
-    console.log('Hello, Stimulus!')
+  load(e) {
+    console.log('Hello, from loading!')
+
+    const modal = new Modal(this.modalTarget)
+    modal.show()
   }
 }

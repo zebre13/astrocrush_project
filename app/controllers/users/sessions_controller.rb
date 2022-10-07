fde67f7a1b2fd1b8c1de37e302e3bef7583212cb
Ghita Aaddaj <aa.ghita@gmail.com>
2022-10-07 12:28:19 +0200

    refactorising the code, creation of services

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -6,8 +6,6 @@ class Users::SessionsController < Devise::SessionsController
   # API_CALL = AstrologyApi.new(ENV["API_UID"], ENV["API_KEY"])
   # after_action :define_coordinates, only: %i[new]
 
-  # Mettre a jour les coordonées de l'utilisateur.
-
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.

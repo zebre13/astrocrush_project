c86fea565e3482e872f5b41ebd5ce668c1aea859
Ghita Aaddaj <aa.ghita@gmail.com>
2022-10-10 17:52:43 +0200

    little bugs fixed

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -13,7 +13,7 @@ class Users::PasswordsController < Devise::PasswordsController
     super(resource)
   end
 
-  The path used after sending reset password instructions
+  # The path used after sending reset password instructions
   def after_sending_reset_password_instructions_path_for(resource_name)
     super(resource_name)
   end

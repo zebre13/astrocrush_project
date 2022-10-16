beefb005e12728f65b0467471bba68fb0d2d2afa
Boris <95616326+zebre13@users.noreply.github.com>
2022-10-16 20:37:07 +0200

    little fix on migration

--- db/migrate/20221016152849_remove_mate_sun_reports_from_users.rb
+++ db/migrate/20221016152849_remove_mate_sun_reports_from_users.rb
@@ -1,5 +1,5 @@
 class RemoveMateSunReportsFromUsers < ActiveRecord::Migration[6.1]
   def change
-    remove_column :users, :mate_sun_reports
+    remove_column :users, :mate_sun_reports, :text
   end
 end

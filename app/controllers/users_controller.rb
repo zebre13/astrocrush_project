6c64ede86beb2c1011d542975849b56abbf3963d
Boris <95616326+zebre13@users.noreply.github.com>
2022-10-28 14:57:15 +0200

    translated chart section of astroboard in FR

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,7 +55,7 @@ class UsersController < ApplicationController
     hash_planets.each do |planet, hash|
       if @my_zodiac[zodiac_index] == hash[:sign]
         data_to_display["sign"] = hash[:sign]
-        data_to_display["planet"] = planet.to_s.upcase
+        data_to_display["planet"] = planet.to_s.capitalize
         data_to_display["house"] = hash[:house]
         data_to_display["logo"] = LOGOS[planet]
         planets << data_to_display

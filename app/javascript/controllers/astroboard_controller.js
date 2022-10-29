526255c39dd44ed8caf8737d737c9fc1a9ec0c30
Boris <95616326+zebre13@users.noreply.github.com>
2022-10-30 00:27:17 +0200

    wip

--- app/javascript/controllers/astroboard_controller.js
+++ app/javascript/controllers/astroboard_controller.js
@@ -26,55 +26,52 @@ export default class extends Controller {
   horoscope() {
     this.hover(this.horoscope_menuTarget)
     this.horoscopeTarget.classList.remove("d-none");
-
-    this.profileTarget.classList.add("d-none");
-    this.scoreTarget.classList.add("d-none");
-
     this.horoscope_menuTarget.classList.add("lead");
 
+    this.profileTarget.classList.add("d-none");
     this.profile_menuTarget.classList.remove("lead");
+
+    this.scoreTarget.classList.add("d-none");
     this.score_menuTarget.classList.remove("lead");
   }
 
   profile() {
     this.hover(this.profile_menuTarget)
     this.profileTarget.classList.remove("d-none");
-
-    this.horoscopeTarget.classList.add("d-none");
-    this.scoreTarget.classList.add("d-none");
-
     this.profile_menuTarget.classList.add("lead");
 
+    this.horoscopeTarget.classList.add("d-none");
     this.horoscope_menuTarget.classList.remove("lead");
+
+    this.scoreTarget.classList.add("d-none");
     this.score_menuTarget.classList.remove("lead");
   }
 
   circle() {
     this.circleTarget.classList.remove("d-none");
-    this.tableTarget.classList.add("d-none");
-
     this.circle_menuTarget.classList.add("lead");
+
+    this.tableTarget.classList.add("d-none");
     this.table_menuTarget.classList.remove("lead");
   }
 
   table() {
     this.tableTarget.classList.remove("d-none");
-    this.circleTarget.classList.add("d-none");
-
     this.table_menuTarget.classList.add("lead");
+
+    this.circleTarget.classList.add("d-none");
     this.circle_menuTarget.classList.remove("lead");
     }
 
   score() {
     this.hover(this.score_menuTarget)
     this.scoreTarget.classList.remove("d-none");
-
-    this.horoscopeTarget.classList.add("d-none");
-    this.profileTarget.classList.add("d-none");
-
     this.score_menuTarget.classList.add("lead");
 
+    this.horoscopeTarget.classList.add("d-none");
     this.horoscope_menuTarget.classList.remove("lead");
+
+    this.profileTarget.classList.add("d-none");
     this.profile_menuTarget.classList.remove("lead");
   }
 }

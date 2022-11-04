d618d64ab00cd1bb3463010d1b92eedceb0cb253
Boris <95616326+zebre13@users.noreply.github.com>
2022-11-04 15:09:17 +0100

    replaced planets with horoscope_data and deleted personality_report in seed and registration

--- app/services/astroprofil.rb
+++ app/services/astroprofil.rb
@@ -2,27 +2,12 @@ class Astroprofil
   API_CALL = AstrologyApi.new(ENV["API_UID"], ENV["API_KEY"])
 
   def profil(user)
-    horoscope = API_CALL.horoscope(user.birth_date, user.birth_hour, user.latitude.to_f, user.longitude.to_f)
-    user.sign = horoscope['planets'].first['sign']
-    user.rising = horoscope['houses'].first['sign']
-    user.moon = horoscope['planets'][1]['sign']
-    user.horoscope_data = horoscope
-    user.planets = planets(horoscope)
+    user.horoscope_data = API_CALL.horoscope(user.birth_date, user.birth_hour, user.latitude.to_f, user.longitude.to_f)
+    user.sign = user.horoscope_data['planets'].first['sign']
+    user.rising = user.horoscope_data['houses'].first['sign']
+    user.moon = user.horoscope_data['planets'][1]['sign']
     user.wheel_chart = API_CALL.wheel_chart(user.birth_date, user.birth_hour, user.latitude.to_f, user.longitude.to_f, "#2E3A59", "#ffffff", "#ffffff", "#2E3A59")
-    user.personality_report = API_CALL.personality_report(user.birth_date, user.birth_hour, user.latitude.to_f, user.longitude.to_f)
     user.timezone = API_CALL.time_zone(user.latitude.to_f, user.longitude.to_f, user.birth_date)
     user.save!
   end
-
-  private
-
-  def planets(horoscope)
-    planets = { Sun: {}, Moon: {}, Mars: {}, Mercury: {}, Jupiter: {}, Venus: {}, Saturn: {}, Uranus: {}, Neptune: {}, Pluto: {} }
-    planets.each_key do |key|
-      horoscope['planets'].each do |element|
-        planets[key] = { sign: element['sign'], house: element['house'] } if element['name'] == key.to_s
-      end
-    end
-  end
-
 end

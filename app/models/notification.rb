c80ec73401a3f97b813d0fabdbf68361fa822fc4
Ghita Aa <aa.ghita@gmail.com>
2022-11-22 14:07:49 +0100

    NotificationRelayJob commented

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -3,5 +3,5 @@ class Notification < ApplicationRecord
   belongs_to :recipient, class_name: "User"
   belongs_to :notifiable, polymorphic: true
 
-  after_commit -> { NotificationRelayJob.perform_later(self) }
+  # after_commit -> { NotificationRelayJob.perform_later(self) }
 end

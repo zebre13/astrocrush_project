d16ba516fea33972b5836d0139a8cda394ad5636
Boris <95616326+zebre13@users.noreply.github.com>
2022-11-23 15:43:43 +0100

    from free to pro version of deepl

--- app/services/translation.rb
+++ app/services/translation.rb
@@ -2,7 +2,7 @@ class Translation
   def initialize
     DeepL.configure do |config|
       config.auth_key = ENV['DEEPL_API_KEY']
-      config.host = 'https://api-free.deepl.com' # Default value is 'https://api.deepl.com'
+      config.host = 'https://api.deepl.com' # When using free version: 'https://api-free.deepl.com'
     end
   end
 
@@ -10,9 +10,3 @@ class Translation
     DeepL.translate text, 'EN', 'FR'
   end
 end
-
-# translated_reports = reports.transform_values do |report|
-#     report.transform_values { |item| Translation.new.to_fr(item).text }
-# end
-
-# p translated_reports

55e453e9d77221ab2a9dd4594b3c50e8bca8d8e7
Ghita Aa <aa.ghita@gmail.com>
2022-11-26 15:37:07 +0100

    haversine distance added to the geocode

--- app/services/geocode.rb
+++ app/services/geocode.rb
@@ -1,46 +1,31 @@
 class Geocode
-  def coordinates(user)
-    #  Assigne local_lon et local_lat
-    p user.last_sign_in_ip
-    data = Geocoder.search(user.last_sign_in_ip.to_s).first.coordinates
-    p data, "this is data"
-    user.local_lat = data[0]
-    user.local_lon = data[1]
-    p "=> #{user.local_lat}<= is user local_lat "
-    p "=> #{user.local_lon}<= is user local_lon "
+  def self.coordinates(user)
+    coordinates = Geocoder.search(user.last_sign_in_ip.to_s).first.coordinates
+    user.local_lat = coordinates[0]
+    user.local_lon = coordinates[1]
     user.coordinates_updated_today = true
     user.save!
   end
 
-  def calculate_distance(user, mate)
-    coordinates(mate) unless mate.coordinates_updated_today == true
-    distance = Geocoder::Calculations.distance_between([user.local_lat, user.local_lon], [mate.local_lat, mate.local_lon], options = {})
-    p "=> #{distance} <= km  betwen user #{user.id} and mate #{mate.id}"
-    return distance unless distance.nil?
+  def self.haversine_distance(user, mate, miles: false)
+    if user.local_lon && user.local_lat && mate.local_lat && mate.local_lon
+      d_lat = (mate.local_lat - user.local_lat) * Math::PI / 180
+      d_lon = (mate.local_lon - user.local_lon) * Math::PI / 180
+
+      a = (Math.sin(d_lat / 2) *
+      Math.sin(d_lat / 2) +
+      Math.cos(user.local_lat * Math::PI / 180) *
+      Math.cos(mate.local_lat * Math::PI / 180) *
+      Math.sin(d_lon / 2) * Math.sin(d_lon / 2))
+      c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+
+      6371 * c * (miles ? 1 / 1.6 : 1)
+    end
   end
-
-  def haversine_distance(lat_a, lon_a, lat_b, lon_b, miles: false)
-    # TODO
-    d_lat = (lat_b - lat_a) * Math::PI / 180
-    d_lon = (lon_b - lon_a) * Math::PI / 180
-
-    a = Math.sin(d_lat / 2) *
-        Math.sin(d_lat / 2) +
-        Math.cos(lat1 * Math::PI / 180) *
-        Math.cos(lat2 * Math::PI / 180) *
-        Math.sin(d_lon / 2) * Math.sin(d_lon / 2)
-    c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
-
-    return 6371 * c * (miles ? 1 / 1.6 : 1)
-  end
-
-  # private
-
-  # def latitude(ip)
-  #   Geocoder.search(ip).first.coordinates[0]
-  # end
-
-  # def longitude(ip)
-  #   return Geocoder.search(ip).first.coordinates[1]
+  # def self.calculate_distance(user, mate)
+  #   coordinates(mate) unless mate.coordinates_updated_today == true
+  #   distance = Geocoder::Calculations.distance_between([user.local_lat, user.local_lon], [mate.local_lat, mate.local_lon], options = {})
+  #   p "=> #{distance} <= km  betwen user #{user.id} and mate #{mate.id}"
+  #   return distance unless distance.nil?
   # end
 end
